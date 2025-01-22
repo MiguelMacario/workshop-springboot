adc4215dc4ed898743bc4c68e7f41ef1c01b64b3
Miguel <miguel251028@gmail.com>
2025-01-22 19:18:10 -0300

    Exception handling - findById

--- src/main/java/com/macario/course/services/UserService.java
+++ src/main/java/com/macario/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.macario.course.services;
 
 import com.macario.course.entities.User;
 import com.macario.course.repositories.UserRepository;
+import com.macario.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(long id) {
        Optional<User> obj = userRepository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
